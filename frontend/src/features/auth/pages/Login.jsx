93c6f7e2046ccde4fadd3dbc22339a0e25d7c5e1
Citu <citukumari388@gmail.com>
2026-07-04 20:43:40 +0530

    feat: implement login page design

--- frontend/src/features/auth/pages/Login.jsx
+++ frontend/src/features/auth/pages/Login.jsx
@@ -1,7 +1,41 @@
 // import React from "react";
+import "../auth.form.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <main>
+      <div className="form-container">
+        <h1>Login</h1>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="Enter email address"
+              name="email"
+            ></input>
+          </div>
+
+          <div className="input-group">
+            <label htmlFor="password">Email</label>
+            <input
+              type="password"
+              id="password"
+              placeholder="Enter password"
+              name="password"
+            ></input>
+          </div>
+
+          <button className="button primary-button">Login</button>
+        </form>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
